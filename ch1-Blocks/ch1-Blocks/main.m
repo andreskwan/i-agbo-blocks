94c701d521ed89affcdfa3eb4be25a861322c810
ANDRES KWAN <andreskwan@gmail.com>
2016-02-01 00:15:38 -0500

    replace with comman line app

---
+++ ch1-Blocks/ch1-Blocks/main.m
@@ -0,0 +1,37 @@
+//
+//  main.m
+//  ch1-Blocks
+//
+//  Created by Andres Kwan on 2/1/16.
+//  Copyright © 2016 Kwan-abgo-jedy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        // NSArray
+        NSArray *list = @[@1,@2,@3];
+        
+        //for de C del abuelo
+        for (int i = 0; i < [list count]; i++) {
+            NSLog(@"Para el index(int): %d el valor retornado es %ld", i, (long)[[list objectAtIndex:i] integerValue]);
+        }
+        
+        //fast iteration
+        //es el mas rapido
+        //id (no se el tipo de lo que esta dentro del array
+        for (id element in list) {
+            NSLog(@"El elemento en la lista es %@", element);
+        }
+        
+        //iterar con bloques
+        [list enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            NSLog(@"El elemento para el indice %lu en la lista es %@", (unsigned long)idx, obj);
+        }];
+
+    }
+    return 0;
+}
