3533f59a93d70c7246b74244a888ea4d8cfc9c8c
Andres Kwan <andreskwan@gmail.com>
2016-09-13 23:40:31 -0500

    [NSOperation - image Filter] - [CoreImage] - [creating class]

---
+++ ch1-DescargaImagenGemelas/ch1-DescargaImagenGemelas/Operations/AKOImageFilter.h
@@ -0,0 +1,13 @@
+//
+//  AKOImageFilter.h
+//  ch1-DescargaImagenGemelas
+//
+//  Created by Andres Kwan on 9/13/16.
+//  Copyright © 2016 Kwan-abgo-jedy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AKOImageFilter : NSOperation
+
+@end
---
+++ ch1-DescargaImagenGemelas/ch1-DescargaImagenGemelas/Operations/AKOImageFilter.m
@@ -0,0 +1,18 @@
+//
+//  AKOImageFilter.m
+//  ch1-DescargaImagenGemelas
+//
+//  Created by Andres Kwan on 9/13/16.
+//  Copyright © 2016 Kwan-abgo-jedy. All rights reserved.
+//
+
+#import "AKOImageFilter.h"
+#import "ViewController.h"
+
+@interface AKOImageFilter ()
+@property (nonatomic, strong) ViewController * mainVC;
+@end
+
+@implementation AKOImageFilter
+
+@end
